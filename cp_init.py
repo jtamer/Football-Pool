033cc16b7d9149387fb67e8f1cf070b318ea19dc
Jim Tamer <jim.tamer@comcast.net>
2017-08-04 12:24:06 -0500

    Add files via upload

--- cp_init.py
+++ cp_init.py
@@ -1,9 +1,5 @@
 CUR_WEEK = 'Week1'  # current week of schedule
 
-TOT_WEEKS = 17  # number of weeks in season
-
-MAX = 16  # max score for the top pick. 
-
 PATTERNS_PATH = '/Users/jimtamer/Modules/Football/patterns.txt'
 
 PLAYERS_PATH = '/Users/jimtamer/Modules/Football/players.txt'
