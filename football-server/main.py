3d94cf1b5ee4e0933fb12c2ed96c9be5e5280d37
Jim Tamer <jim.tamer@comcast.net>
2017-08-09 14:10:09 -0500

    setup of local web server

---
+++ football-server/main.py
@@ -0,0 +1,42 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        with open('footballpool.html') as f:
+            self.response.write(f.read())
+
+class ScheduleJSPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        with open('schedule.js') as f:
+            self.response.write(f.read())
+
+class JSONdownloadPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/octet-stream'
+        self.response.write('{"["Al &amp; Bob","Week 1"]":{"player":"Al &amp; Bob","week":"Week 1","picks":["Titans","Dolphins","Ravens","Patriots","Bills"]},"["Jim T","Week 1"]":{"player":"Jim T","week":"Week 1","picks":["Seahawks","Panthers","Ravens"]}}')
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/footballpool.html', MainPage),
+    ('/schedule.js', ScheduleJSPage),
+    ('/picks.json', JSONdownloadPage),
+], debug=True)
