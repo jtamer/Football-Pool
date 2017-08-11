d6dd216bb7c3b52c57931b63255e7142c72bd3d8
Jim Tamer <jim.tamer@comcast.net>
2017-08-10 21:18:10 -0500

    Fixed json path error

--- cp_init.py
+++ cp_init.py
@@ -8,4 +8,4 @@ PATH = '/Users/jimtamer/Documents/GitHub/Football-Pool'
 PATTERNS_PATH = os.path.join(PATH, 'patterns.txt')
 PLAYERS_PATH = os.path.join(PATH, 'players.txt')
 SCHED_PATH = os.path.join(PATH, 'Schedule.txt')
-JSON_PATH = os.path.join(PATH, 'picks.json')
+JSON_PATH = os.path.join(PATH, 'football-server/picks.json')
